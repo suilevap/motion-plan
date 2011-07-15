1e8c12f138abeaa53241eae0d54b1397e3dd5a67
suilevap <suilevap@localhost>
2011-07-15 13:59:02 +0300

    Added simple timer for performance testing

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,22 +2,29 @@
 //
 
 #include "stdafx.h"
+#include <time.h>
+
 #include <Interface.h>
 #include <FileStruct.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
+	clock_t time0, time1, times;
 	//FileStruct f("map2.txt");
 
 	//char * result = Action(f.GetBuffer());
 	//printf(result);
-	for (int i=0; i < 100000; i++)
+	TestPerformance(true);
+	times = clock();
+	time0 = times;
+	for (int i=1; i < 100000; i++)
 	{
-		TestPerformance();
+		TestPerformance(false);
 		if (i%1000 == 0)
 		{
-			printf("%3dk\n",i/1000);
+			time1 = clock();
+			printf("%3dk time: %4d, avg time: %4d\n",i/1000, (time1 - time0), (time1 - times)/(i/1000) );
+			time0 = time1;
 		}
 	}
 	getchar();	

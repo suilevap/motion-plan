c371f96fba6536ad6306512ebc17d924ea6f2b85
suilevap <suilevap@localhost>
2011-07-27 18:22:35 +0300

    BUILD FAILED: code in intermediate state Issue 2: Refactored CellQueue and PathPoint (now PathNode);

--- MotionPlan/BasePathFinder.h
+++ MotionPlan/BasePathFinder.h
@@ -5,10 +5,10 @@
 
 #include "MapView.h"
 #include <vector>
-//#include "PathPoint.h"
+#include "PathNode.h"
 //#include "Point.h"
 //#include "Path.h"
-//#include "CellQueue.h"
+#include "CellQueue.h"
 namespace Astar
 {
 
@@ -32,25 +32,25 @@ private:
 	PointInfo _startPoint;
 	NodeInfo _goal;
 	NodeInfo _start;
-	CellQueue* _queue;
+	CellQueue<NodeInfo, CostInfo>* _queue;
 	//void DirToXY(int dir, int* x, int* y);
 	//int XYToDir(int x, int y);
 	//float ComputeCost(int x0, int y0, int x1, int y1);
 	//float GetParentDist(int x, int y);
 	bool CheckNeighbor(int index, int dx, int dy);
 	float GetDistance(int index, int dx, int dy);
-	float GetEstimateDistance(int index);
+	float GetEstimateDistance(NodeInfo index);
 	Path* ExtractPath();
-	void Step(int index);
+	void Step(NodeInfo index);
 
 public:
-	PathFinder(Map<int>* map);
+	PathFinder(MapView<PointInfo, CellType, NodeInfo, CostInfo>* map);
 	~PathFinder();
 
-	Path* Find(int x, int y, int goalX, int goalY, float estimateAlpha);
+	Path* Find(PointInfo start, PointInfo goal);
 	
-	//only for debug purpose
-	Map<float>* GetMapDist(){ return _mapDist;};
+	////only for debug purpose
+	//std::vector<float>* GetMapDist(){ return _mapDist;};
 };
 
 #include "BasePathFinder.inl"

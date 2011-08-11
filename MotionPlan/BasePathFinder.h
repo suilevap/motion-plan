8cf197b008c61729713ec43c6bcdeb72797638d3
suilevap <suilevap@gmail.com>
2011-08-12 01:03:23 +0300

    Starts issue 12 Spli find path metho to several methods. and Added base abstract PathFinder

--- MotionPlan/BasePathFinder.h
+++ MotionPlan/BasePathFinder.h
@@ -10,6 +10,7 @@
 #include "EdgeInfo.h"
 #include "Path.h"
 #include "NodeState.h"
+#include "PathFinder.h"
 
 
 namespace AStar
@@ -19,38 +20,44 @@ template<
 	typename PointInfo, 
 	typename CellType, 	
 	typename CostInfo> 
-class BasePathFinder
+class BasePathFinder : public PathFinder<PointInfo, CellType, int, CostInfo>
 {
 public:
 	typedef int NodeInfo;
 
+protected:	
+	//MapView<PointInfo, CellType, NodeInfo, CostInfo>* _map;
 
-private:	
-	MapView<PointInfo, CellType, NodeInfo, CostInfo>* _map;
 	std::vector<NodeState<NodeInfo,CostInfo>> _mapCost;
 
 	std::vector<EdgeInfo<NodeInfo,CostInfo>> _neighbors;
 	
 	PointInfo _goalPoint;
 	PointInfo _startPoint;
-	NodeInfo _goal;
-	NodeInfo _start;
+
 	CellQueue<NodeInfo, CostInfo>* _queue;
 
 	//for debug
 	MapView<PointInfo, CellType, NodeInfo, CostInfo>* _mapForStateDebug;
 
-	bool CheckNeighbor(NodeInfo& node, EdgeInfo<NodeInfo, CostInfo>& edge);
+	bool CheckNeighbor(NodeInfo& node, EdgeInfo<NodeInfo, CostInfo>& edge, NodeInfo& goal);
 	CostInfo GetDistance(NodeInfo& node, EdgeInfo<NodeInfo, CostInfo>& edge);
-	CostInfo GetEstimateDistance(NodeInfo& node);
-	Path<PointInfo>* ExtractPath();
-	void Step(NodeInfo index);	
+	CostInfo GetEstimateDistance(NodeInfo& node1, NodeInfo& node2);
+	Path<PointInfo>* ExtractPath(NodeInfo& toPoint);
+	void Step(NodeInfo& index, NodeInfo& goal);	
 
+	virtual bool IsGoal(NodeInfo& goal);
 public:
 	BasePathFinder(MapView<PointInfo, CellType, NodeInfo, CostInfo>* map);
 	~BasePathFinder();
 
-	Path<PointInfo>* Find(PointInfo start, PointInfo goal);
+	//Path<PointInfo>* Find(PointInfo start, PointInfo goal);
+
+	virtual void Init(MapView<PointInfo, CellType, NodeInfo, CostInfo>* map);
+	virtual void FindStart(NodeInfo start, NodeInfo goal);
+	virtual NodeInfo FindStep();
+	virtual bool FindIsPathExists(NodeInfo& node) ;
+	virtual Path<PointInfo>* FindEnd(NodeInfo& curNode);
 	
 	////only for debug purpose
 	//std::vector<float>* GetMapDist(){ return _mapDist;};

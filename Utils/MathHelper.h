8aa80d65a8f070b42f32bfac326b89f7240ecde4
suilevap <suilevap@localhost>
2011-08-25 17:49:07 +0300

    Added MathHelper

---
+++ Utils/MathHelper.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef UTILS_MATHHELPER_H_
+#define UTILS_MATHHELPER_H_
+
+class MathHelper
+{
+public:
+    static unsigned int UpperPowerOfTwo(unsigned int v);
+
+    static unsigned int UpperRankOfTwo(unsigned int v);
+
+};
+
+#endif //UTILS_MATHHELPER_H_

c72231416a9b4258fb60b2dbaaca15928ca375f2
suilevap <suilevap@localhost>
2011-09-22 17:23:22 +0300

    Added some unit test for NavRect class

--- MotionPlanTests/NavigationRectangleTests.cpp
+++ MotionPlanTests/NavigationRectangleTests.cpp
@@ -9,7 +9,7 @@ TEST(NavigationRectangle, CheckNeighboors)
     AStar::NavigationRectangle<Point<int>, int, float> rect2(Point<int>(5,5),Point<int>(15,15));
     bool result;
     
-    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,2),Point<int>(5,4));
+    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,2),Point<int>(5,3));
     result = rect1.IsNeighbor(rect2);
     EXPECT_FALSE(result);
 
@@ -23,7 +23,25 @@ TEST(NavigationRectangle, CheckNeighboors)
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,16),Point<int>(5,27));
     result = rect1.IsNeighbor(rect2);
+    EXPECT_TRUE(result);
+
+    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(17,17),Point<int>(25,27));
+    result = rect1.IsNeighbor(rect2);
     EXPECT_FALSE(result);
+    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(16,17),Point<int>(25,27));
+    result = rect1.IsNeighbor(rect2);
+    EXPECT_FALSE(result);
+    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(16,14),Point<int>(25,27));
+    result = rect1.IsNeighbor(rect2);
+    EXPECT_TRUE(result);
+
+    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(10,15),Point<int>(25,27));
+    result = rect1.IsNeighbor(rect2);
+    EXPECT_TRUE(result);
+
+    rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,14),Point<int>(18,4));
+    result = rect1.IsNeighbor(rect2);
+    EXPECT_TRUE(result);
 
 }
 

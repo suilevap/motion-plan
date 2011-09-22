f99e1c0ce32f072f1eb5ff38fa06064387d7c96e
suilevap <suilevap@localhost>
2011-09-22 17:30:41 +0300

    Implement method NavigationRectangle.FindNeighbors

--- MotionPlanTests/NavigationRectangleTests.cpp
+++ MotionPlanTests/NavigationRectangleTests.cpp
@@ -10,37 +10,37 @@ TEST(NavigationRectangle, CheckNeighboors)
     bool result;
     
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,2),Point<int>(5,3));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_FALSE(result);
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,2),Point<int>(5,7));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_TRUE(result);
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,10),Point<int>(5,12));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_TRUE(result);
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,16),Point<int>(5,27));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_TRUE(result);
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(17,17),Point<int>(25,27));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_FALSE(result);
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(16,17),Point<int>(25,27));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_FALSE(result);
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(16,14),Point<int>(25,27));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_TRUE(result);
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(10,15),Point<int>(25,27));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_TRUE(result);
 
     rect2 = AStar::NavigationRectangle<Point<int>, int, float>(Point<int>(2,14),Point<int>(18,4));
-    result = rect1.IsNeighbor(rect2);
+    result = rect1.IsNeighbor(&rect2);
     EXPECT_TRUE(result);
 
 }
